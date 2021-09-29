f94944a449d31530dfb42e86be15ce596d37e83d
Quentin ADADAIN <61509041+Rolls47@users.noreply.github.com>
2021-09-29 07:39:42 +0200

    Add tenant cards and rentreceipt by tenant (#10)

--- src/Controller/RentReceiptController.php
+++ src/Controller/RentReceiptController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\RentReceipt;
+use App\Entity\Tenant;
 use App\Form\RentReceiptType;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,5 +34,26 @@ class RentReceiptController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/{locataireFirstName}-{locataireLastName}-{id}", name="show")
+     */
+    public function getRentReceiptByTenant(Tenant $tenant, EntityManagerInterface $em, PaginatorInterface $paginator, Request $request): Response
+    {
+        $rentReceiptRepository = $em->getRepository('App:RentReceipt');
+
+        $data = $rentReceiptRepository->findBy(['tenant' => $tenant], ['date' => 'desc']);
+
+        $rentReceipts = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            12
+        );
+        return $this->render('show/tenantRentReceipt.html.twig', [
+            'tenant' => $tenant,
+            'rentReceipts' => $rentReceipts,
+        ]);
+    }
+
 
 }
