6a65f9128b5f2d4b6abdc373a13f5532576d19f4
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-09-29 08:29:57 +0200

    feat(pdf): Add knp snappy-bundle and generate pdf

--- src/Controller/RentReceiptController.php
+++ src/Controller/RentReceiptController.php
@@ -5,8 +5,13 @@ namespace App\Controller;
 use App\Entity\RentReceipt;
 use App\Entity\Tenant;
 use App\Form\RentReceiptType;
+use App\Repository\RentReceiptRepository;
+use App\Repository\TenantRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Knp\Component\Pager\PaginatorInterface;
+use Knp\Snappy\Pdf;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -53,6 +58,32 @@ class RentReceiptController extends AbstractController
             'tenant' => $tenant,
             'rentReceipts' => $rentReceipts,
         ]);
+    }
+
+    /**
+     * @Route("/pdf/pdf-{id}", name="pdf")
+     * @throws Exception
+     */
+    public function pdfAction(RentReceiptRepository $rentReceiptRepository, Pdf $knpSnappyPdf, int $id)
+    {
+        $rentReceipt = $rentReceiptRepository->findOneBy(['id' => $id]);
+        $html = $this->renderView('/_Components/pdf/rentReceipt.html.twig', [
+            'user' => $this->getUser(),
+            'rentReceipt' => $rentReceipt,
+        ]);
+
+        $knpSnappyPdf->setOption('encoding', 'UTF-8');
+
+        $date = date_format($rentReceipt->getDate(), 'm-Y');
+        $tenant = mb_strtolower($rentReceipt->getTenant(), 'UTF-8');
+        $tenant = str_replace(' ', '-', $tenant);
+        $tenant = TenantRepository::pdf_str_replace($tenant);
+        dump($date, $tenant);
+
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html),
+            "loyer-$tenant-$date.pdf"
+        );
     }
 
 
