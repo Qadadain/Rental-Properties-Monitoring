d51bb3770d7624a80e385c68e9dda57d2316ac65
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-10-23 20:26:26 +0200

    Dockerfile prod

--- docker/script/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cp /var/www/html/.env /var/www/html/.env.local
-echo 'DATABASE_URL="mysql://root:rootroot@db:3306/RPM?serverVersion=5.7"' >> /var/www/html/.env.local
-composer install
-yarn install
-yarn encore dev
