2a2925c7882d244cd79c2efa86e071aea42be985
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-10-23 19:50:40 +0200

    Dockerfile prod

--- docker/script/install.sh
+++ docker/script/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cp .env .env.local
+cp /var/www/html/.env /var/www/html/.env.local
 echo 'DATABASE_URL="mysql://root:rootroot@db:3306/RPM?serverVersion=5.7"' >> /var/www/html/.env.local
 composer install
 yarn install
