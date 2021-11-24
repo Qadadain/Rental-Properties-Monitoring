945ad3eaaa26e052cd9b50402d2e8d0e3b76c07b
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-11-24 15:29:29 +0100

    misc

--- docker/script/init.sh
+++ docker/script/init.sh
@@ -4,7 +4,6 @@ while ! mysqladmin ping -hdb --silent; do
     sleep 1
 done
 
-php /var/www/html/bin/console doctrine:database:create
 php /var/www/html/bin/console doctrine:migrations:migrate -n
 php /var/www/html/bin/console doctrine:fixtures:load -n
 
