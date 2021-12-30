81ec3704304b1c014c7cbae84afdef90a4ff4e5c
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-12-30 18:54:09 +0100

    Change click trigger on date form

--- src/Controller/TenantController.php
+++ src/Controller/TenantController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/tenant", name="tenant_")
+ * @Route("/locataire", name="tenant_")
  */
 class TenantController extends AbstractController
 {
