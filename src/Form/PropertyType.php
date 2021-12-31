a4f57727cf1a3d5ae7a6b9a9401e8e38aa5fc74d
Quentin ADADAIN <quentin.adadain@gmail.com>
2021-12-31 17:24:49 +0100

    Fix property form

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Property;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,6 +22,9 @@ class PropertyType extends AbstractType
             ])
             ->add('address', TextType::class, [
                 'label' => 'Adresse :'
+            ])
+            ->add('zipCode', IntegerType::class, [
+                'label' => 'Code postal :'
             ])
             ->add('comment', TextType::class, [
                 'label' => 'Commentaire :'
